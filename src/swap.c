e9004626250ffe1a19af280e0ba094a1d35f8af7
许宜森 <m13277032800_1@163.com>
2018-06-12 21:03:05 +0800

    -swap

---
+++ src/swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    scanf("%d%d",&a,&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("%d %d",a,b);
+    return 0;
+
+}
